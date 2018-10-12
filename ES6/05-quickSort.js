4a41e547c7481b31ba5bde2516deef7971db8a15
askwuxue <972523419@qq.com>
2018-10-12 14:32:40 +0800

    修改快排

--- ES6/05-quickSort.js
+++ ES6/05-quickSort.js
@@ -1,22 +1,48 @@
 let quickSort = (arr) => {
-    if (arr.length === 0) return [];
-    if (arr.length === 1) return arr;
-    let randomNum = Math.floor(Math.random() * arr.length);
-    let currentNum = arr[randomNum];
+    // 数组长度小于2 返回当前
+    if (arr.length < 2) return arr;
+    // 基准索引，选取数组中的中间元素的索引
+    let pivotIndex = Math.floor(Math.random() * arr.length);
+    // 基准元素
+    let pivot = arr.splice(pivotIndex, 1);
+    // 小于等于基准的数组
     let lesser = [];
-    let mid = [];
+    // 大于基准的数组
     let greater = [];
+    // 循环遍历，小于等于随机数的放在小于数组中，大于随机数的放在大于数组中
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < currentNum) {
+        if (arr[i] < pivot) {
             lesser.push(arr[i]);
-        } else if (arr[i] === currentNum) {
-            mid.push(arr[i]);
         } else {
             greater.push(arr[i]);
         }
     }
-    return quickSort(lesser).concat(mid, quickSort(greater));
+    // 进行递归
+    return quickSort(lesser).concat(pivot, quickSort(greater));
 }
+
+// let quickSort = (arr) => {
+//     // 数组长度小于2 返回当前
+//     if (arr.length < 2) return arr;
+//     // 基准索引，选取数组中的中间元素的索引
+//     let pivotIndex = Math.floor(arr.length / 2);
+//     // 基准元素
+//     let pivot = arr.splice(pivotIndex, 1);
+//     // 小于等于基准的数组
+//     let lesser = [];
+//     // 大于基准的数组
+//     let greater = [];
+//     // 循环遍历，小于等于随机数的放在小于数组中，大于随机数的放在大于数组中
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < pivot) {
+//             lesser.push(arr[i]);
+//         } else {
+//             greater.push(arr[i]);
+//         }
+//     }
+//     // 进行递归
+//     return quickSort(lesser).concat(pivot, quickSort(greater));
+// }
 
 let test = (size) => {
     let arr = [];
