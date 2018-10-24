638323ec796cea6c41e18b55d358b9fe0f406f71
askwuxue <972523419@qq.com>
2018-10-24 16:38:31 +0800

    快排 大根堆 。。。堆排序待处理

---
+++ study-arithmetic/HeapSort/src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class HeapSort {
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        // 根据每一个元素 调整为大根堆的过程
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+
+        // heapify的过程 当某个数发生变化时
+        int size = arr.length;
+        swap(arr, 0, --size);
+        while (size > 0) {
+            heapify(arr, 0, size);
+            swap(arr, 0, --size);
+        }
+    }
+
+    // 调整顺序 大根堆的过程
+    private static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    private static void heapify(int[] arr, int index, int size) {
+        int left = index * 2 + 1;
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] < arr[left] ? arr[left] : arr[left + 1];
+            largest = arr[largest] > arr[index] ? arr[largest] : arr[index];
+            if (largest == index) {
+                break;
+            }
+            swap(arr, largest, index);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
--- study-arithmetic/QuickSort/src/Main.java
+++ study-arithmetic/QuickSort/src/Main.java
@@ -1,25 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] arr = new int[] {1,5,6,2,0,9};
+        QuickSort qs = new QuickSort();
+        qs.quickSort(arr);
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i]);
+//        }
+        System.out.println(Arrays.toString(arr));
     }
 }
 
 class QuickSort {
-    public static void quickSort (int[] arr, int L, int R) {
+    public static void quickSort(int[] arr) {
+
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int L, int R) {
         if (L < R) {
-            // 交换随机数与数组中最后一个数
-            swap(arr, (int) Math.random() * (R - L + 1), R);
-            int p[] = partition(arr, L, R);
-            quickSort(arr, L, p[0] - 1);
-            quickSort(arr, p[0] + 1, R);
+            swap(arr, L + (int)Math.random() * (R - L + 1), R);
+            int[] p = partition(arr, L, R);
+            quickSort(arr, 0, p[0] - 1);
+            quickSort(arr, p[1] + 1, R);
         }
     }
 
-    public static int[] partition (int[] arr, int L, int R) {
+    // 进行partition
+    public static int[] partition(int[] arr, int L, int R) {
         int less = L - 1;
         int more = R;
-        while (R < more) {
+        while (L < more) {
             if (arr[L] < arr[R]) {
                 swap(arr, ++less, L++);
             } else if (arr[L] > arr[R]) {
@@ -29,9 +47,11 @@ class QuickSort {
             }
         }
         swap(arr, more, R);
-        return new int[]{less + 1, more};
+        return new int[] {less + 1, more};
     }
-    
+
+
+
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
