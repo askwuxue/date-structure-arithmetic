25ddd13c1cd028df538a5724726cf3b2677a7207
askwuxue <972523419@qq.com>
2018-10-26 17:58:08 +0800

    栈实现队列 队列实现栈

---
+++ study-arithmetic/ArrayStack/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayStack stack = new ArrayStack(10);
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        System.out.println(stack.pop());
+    }
+}
+
+class ArrayStack {
+    private Integer[] arr;
+    private Integer size;
+
+    public ArrayStack(int initSize) {
+        if (initSize < 0) {
+            throw new IllegalArgumentException("the initSize not less 0");
+        }
+        arr = new Integer[initSize];
+        size = 0;
+    }
+
+    // peek 操作
+    public Integer peek() {
+        if (size == 0) {
+            return null;
+        }
+        return arr[size - 1];
+    }
+
+    // push
+    public void push(int num) {
+        if (size == arr.length) {
+            throw new IllegalArgumentException("stack is full");
+        }
+        arr[size++] = num;
+    }
+
+    // pop
+    public int pop() {
+        if (size == 0) {
+            throw new IllegalArgumentException("stack is empty");
+        }
+        return arr[--size];
+    }
+}
