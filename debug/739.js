516496d73fbc80663fe5bed35919b2bdfa621d3d
askwuxue <972523419@qq.com>
2021-05-19 23:37:01 +0800

    formate

--- debug/739.js
+++ debug/739.js
@@ -1,18 +1,13 @@
 var dailyTemperatures = function (temperatures) {
-  let len = temperatures.length;
-  let stack = [];
-  let res = new Array(len).fill(0);
-  for (let i = 0; i < len; i++) {
-    while (
-      stack.length &&
-      temperatures[i] > temperatures[stack[stack.length - 1]]
-    ) {
-      debugger;
-      let index = stack.pop();
-      res[index] = i - index;
+    let len = temperatures.length;
+    let stack = [];
+    let res = new Array(len).fill(0);
+    for (let i = 0; i < len; i++) {
+        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            let index = stack.pop();
+            res[index] = i - index;
+        }
+        stack.push(i);
     }
-    debugger;
-    stack.push(i);
-  }
-  return res;
+    return res;
 };
