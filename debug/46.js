e96aa8e97bd8805c4c96cc4427106c0fa437349e
askwuxue <972523419@qq.com>
2021-06-13 11:06:39 +0800

    have a bug

--- debug/46.js
+++ debug/46.js
@@ -1,7 +1,9 @@
 var permute = function (nums) {
     const res = new Array();
     const path = new Array();
-    const dfs = (nums, res, path) => {
+    const used = new Array(nums.length).fill(false);
+    // console.log(used[0]);
+    const dfs = (nums, res, path, used) => {
         if (path.length === nums.length) {
             res.push([...path]);
             return;
@@ -9,21 +11,18 @@ var permute = function (nums) {
 
         for (let i = 0; i < nums.length; i++) {
             // 如果path中没有该数，则加入path
-            // console.log(nums[i]);
-            console.log(nums[i], path, path.length, path.indexOf(nums[i]));
-            let hasValue = path.indexOf(nums) === -1 ? false : true;
-            // let set = new Set(path);
-            // console.log(set);
-            // console.log(set.has(nums[i]));
-            // if (path.indexOf(nums[i]) === -1) {
+            console.log(used);
+            if (used[i]) {
+                continue;
+            }
             path.push(nums[i]);
-            // }
+            used[i] = true;
             dfs(nums, res, path);
             path.pop();
         }
         return res;
     }
-    return dfs(nums, res, path)
+    return dfs(nums, res, path, used)
 };
 
 const nums = [1, 2, 3];
