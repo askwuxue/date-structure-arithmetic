06a1b0e6d3c22dd6b9ab1dff2c66744ff436977d
askwuxue <972523419@qq.com>
2021-08-29 10:20:52 +0800

    update leetcode

---
+++ debug/145_2.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const postorderTraversal = function (root) {
+  const res = []
+  const stack = []
+  if (root === null) {
+    return res
+  }
+  stack.push(root)
+  while (stack.length) {
+    const node = stack.pop()
+    res.push(node.val)
+    if (node.left) {
+      stack.push(node.left)
+    }
+    if (node.right) {
+      stack.push(node.right)
+    }
+  }
+  return res.reverse()
+}
+postorderTraversal()
