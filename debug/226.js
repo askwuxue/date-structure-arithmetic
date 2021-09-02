105664e0c9ca71c6afe40d3c10988d60f62a1f64
askwuxue <972523419@qq.com>
2021-09-03 07:51:48 +0800

    upadte leetcode

---
+++ debug/226.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function (root) {
+  if (root === null) {
+    return root
+  }
+  const left = invertTree(root.left)
+  const right = invertTree(root.right)
+  root.left = right
+  root.right = left
+  return root
+}
+
+invertTree()
