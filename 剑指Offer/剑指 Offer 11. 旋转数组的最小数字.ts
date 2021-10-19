81ca7d9ac869cd37dd1b9b6cb283c4116b39cebb
askwuxue <972523419@qq.com>
2021-10-20 07:40:48 +0800

    update leetcode

--- 剑指Offer/剑指 Offer 11. 旋转数组的最小数字.ts
+++ 剑指Offer/剑指 Offer 11. 旋转数组的最小数字.ts
@@ -14,23 +14,3 @@ function minArray(numbers: number[]): number {
   }
   return numbers[left]
 };
-
-function minArray(numbers: number[]): number {
-  if (numbers.length === 1) {
-    return numbers[0]
-  }
-  const len = numbers.length
-  let right: number = len - 1
-  let left: number = 0
-  while (left <= right) {
-    let mid = Math.floor(left + (right - left) / 2)
-    if (numbers[mid] < numbers[right]) {
-      right = mid
-    } else if (numbers[mid] > numbers[right]) {
-      left = mid + 1
-    } else {
-      right -= 1
-    }
-  }
-  return numbers[left]
-};
