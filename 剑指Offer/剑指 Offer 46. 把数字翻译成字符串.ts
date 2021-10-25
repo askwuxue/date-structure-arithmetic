55cb7dcab425f94bd6e36d91a56c3d7cc244621e
askwuxue <972523419@qq.com>
2021-10-25 22:39:17 +0800

    update leetcode failed

---
+++ 剑指Offer/剑指 Offer 46. 把数字翻译成字符串.ts
@@ -0,0 +1,16 @@
+function translateNum(num: number): number {
+  const str: string = String(num)
+  let p = 0, q = 0, r = 1
+  const len = str.length
+  // for (let i = 0; i < len; ++i) {
+  //   p = q
+  //   q = r
+  //   r = 0
+  //   r += q
+  //   if (i === 0) {
+  //     continue
+  //   }
+  //   const pre: string = str.substr(i - 1, i + 1)
+  //   if (parseFloat(pre) < )
+  // }
+};
