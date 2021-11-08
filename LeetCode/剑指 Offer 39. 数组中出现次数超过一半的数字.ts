7350ec6e58ade50d8f1c186836cf8ac4ee7bff06
askwuxue <972523419@qq.com>
2021-11-08 22:23:08 +0800

    update

---
+++ LeetCode/剑指 Offer 39. 数组中出现次数超过一半的数字.ts
@@ -0,0 +1,14 @@
+function majorityElement(nums: number[]): number {
+  const min = 0
+  const len = nums.length
+  const count = (index) => {
+
+
+    
+  }
+  while (true) {
+    let random = Math.random() * len
+    random = Math.floor(random)
+    if ()
+  }
+};
