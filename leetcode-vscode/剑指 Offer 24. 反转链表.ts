2a4430caba24c5ec008de4916e48d947f8d7a257
askwuxue <972523419@qq.com>
2021-11-24 19:41:20 +0800

    update

---
+++ leetcode-vscode/剑指 Offer 24. 反转链表.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (head === null || head.next === null) {
+    return head
+  }
+  let pre = null
+  let curr = head
+  while (curr) {
+    let temp = curr.next
+    curr.next = pre
+    pre = curr
+    curr = temp
+  }
+  return pre
+};
