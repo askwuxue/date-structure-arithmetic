f05b655b87e3533236dc6ac4a43800643bb3344f
askwuxue <972523419@qq.com>
2021-11-24 20:16:33 +0800

    update

---
+++ leetcode-vscode/200.岛屿数量.ts
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=typescript
+ *
+ * [200] 岛屿数量
+ */
+
+// @lc code=start
+function dfs(grid, r, c) {
+  const nr = grid.length
+  const nc = grid[0].length
+  grid[r][c] = '0'
+  if ((c - 1 >= 0) && grid[r][c - 1] === '1') {
+    dfs(grid, r, c - 1)
+  }
+  if ((c + 1 < nc) && grid[r][c + 1] === '1') {
+    dfs(grid, r, c + 1)
+  }
+  if ((r - 1 >= 0) && grid[r - 1][c] === '1') {
+    dfs(grid, r - 1, c)
+  }
+  if ((r + 1 < nr) && grid[r + 1][c] === '1') {
+    dfs(grid, r + 1, c)
+  }
+  return
+}
+
+function numIslands(grid: string[][]): number {
+  const nr = grid.length
+  const nc = grid[0].length
+  let landNum = 0
+  if (!nc) {
+    return landNum
+  }
+  for (let i = 0; i < nr; i++) {
+    for (let j = 0; j < nc; j++) {
+      if (grid[i][j] === '1') {
+        dfs(grid, i, j)
+        landNum++
+      }
+    }
+  }
+  return landNum
+};
+// @lc code=end
+
