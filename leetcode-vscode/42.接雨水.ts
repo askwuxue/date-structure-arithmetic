819ad68872d9e90bbd4de6298e83ecdf44b099ff
askwuxue <972523419@qq.com>
2022-03-16 02:03:03 +0800

    update

--- leetcode-vscode/42.接雨水.ts
+++ leetcode-vscode/42.接雨水.ts
@@ -5,26 +5,45 @@
  */
 
 // @lc code=start
+// function trap(height: number[]): number {
+//   const len = height.length
+//   let res = 0
+//   if (len < 2) {
+//     return 0
+//   }
+//   let leftMax: number[] = []
+//   leftMax[0] = height[0]
+//   for (let i = 1; i < len; ++i) {
+//     leftMax[i] = Math.max(height[i], leftMax[i - 1])
+//   }
+//   let rightMax: number[] = []
+//   rightMax[len - 1] = height[len - 1]
+//   for (let i = len - 2; i >= 0; --i) {
+//     rightMax[i] = Math.max(height[i], rightMax[i + 1])
+//   }
+//   for (let i = 0; i < len; ++i) {
+//     res += Math.min(leftMax[i], rightMax[i]) - height[i]
+//   }
+//   return res
+// };
 function trap(height: number[]): number {
-  const len = height.length
-  let res = 0
-  if (len < 2) {
-    return res
+  const len = height.length;
+  let left = 0,
+    right = len - 1;
+  let leftMax = 0,
+    rightMax = 0;
+  let res = 0;
+  while (left < right) {
+    leftMax = Math.max(leftMax, height[left]);
+    rightMax = Math.max(rightMax, height[right]);
+    if (height[left] < height[right]) {
+      res += leftMax - height[left];
+      ++left;
+    } else {
+      res += rightMax - height[right];
+      --right;
+    }
   }
-  const leftMax = []
-  leftMax[0] = height[0]
-  for (let i = 1; i < len; i++) {
-    leftMax[i] = Math.max(leftMax[i - 1], height[i])
-  }
-  const rightMax = []
-  rightMax[len - 1] = height[len - 1]
-  for (let i = len - 2; i >= 0; i--) {
-    rightMax[i] = Math.max(rightMax[i + 1], height[i])
-  }
-  for (let i = 0; i < len; i++) {
-    res += Math.min(leftMax[i], rightMax[i]) - height[i]
-  }
-  return res
-};
+  return res;
+}
 // @lc code=end
-
