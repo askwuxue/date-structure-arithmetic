17add4492a0859c8d295e91ce418211d11ad6f67
askwuxue <972523419@qq.com>
2022-03-17 02:04:27 +0800

    update

--- leetcode-vscode/6.z-字形变换.ts
+++ leetcode-vscode/6.z-字形变换.ts
@@ -7,30 +7,28 @@
 // @lc code=start
 function convert(s: string, numRows: number): string {
   if (numRows < 2) {
-    return s
-  } 
-  const LEN = s.length
-  const MINROW = Math.min(LEN, numRows)
-  let isGoDown = false
+    return s;
+  }
+  const LEN = s.length;
+  let isGoDown = false;
   // 当前处于第几行
-  let currentRow = 0
-  let resArr = []
-  let res = ''
+  let currentRow = 0;
+  let resArr = [];
+  let res = "";
   // 几行，就需要几个数组存储对应的数据
-  for (let i = 0; i < MINROW; ++i) {
-    resArr[i] = []
+  for (let i = 0; i < numRows; ++i) {
+    resArr[i] = [];
   }
   for (let i = 0; i < LEN; i++) {
-    resArr[currentRow].push(s[i])
+    resArr[currentRow].push(s[i]);
     if (currentRow === 0 || currentRow === numRows - 1) {
-      isGoDown = !isGoDown
+      isGoDown = !isGoDown;
     }
-    currentRow += (isGoDown ? 1 : -1)
+    currentRow += isGoDown ? 1 : -1;
   }
   for (const rowArr of resArr) {
-    res += rowArr.join('')
+    res += rowArr.join("");
   }
-  return res
-};
+  return res;
+}
 // @lc code=end
-
