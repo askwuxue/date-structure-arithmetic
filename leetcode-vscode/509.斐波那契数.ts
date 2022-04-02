cd390e00d6956d56bfc763d18a11c4ac74f2046d
askwuxue <972523419@qq.com>
2022-04-02 12:18:56 +0800

    update leetcode112, 509

---
+++ leetcode-vscode/509.斐波那契数.ts
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=typescript
+ *
+ * [509] 斐波那契数
+ *
+ * https://leetcode-cn.com/problems/fibonacci-number/description/
+ *
+ * algorithms
+ * Easy (67.05%)
+ * Likes:    431
+ * Dislikes: 0
+ * Total Accepted:    359.8K
+ * Total Submissions: 539.8K
+ * Testcase Example:  '2'
+ *
+ * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ *
+ *
+ * F(0) = 0，F(1) = 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ *
+ *
+ * 给定 n ，请计算 F(n) 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：n = 4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= n <= 30
+ *
+ *
+ */
+
+// @lc code=start
+function fib(n: number): number {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  let prev = 1,
+    curr = 1;
+  for (let i = 3; i <= n; ++i) {
+    let sum = prev + curr;
+    prev = curr;
+    curr = sum;
+  }
+  return curr;
+}
+// @lc code=end
